42f7483d29a983396250e5cf38738df639cadd8d
Estiven <Estiven@DESKTOP-ENN0B09>
2018-12-05 17:05:34 -0600

    Agregación de clases

--- TareaProgramadaPEM12/src/Test.java
+++ TareaProgramadaPEM12/src/Test.java
@@ -1,20 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
- * @author Estiven
+ * @author Estiven Álvarez
+ * @author Priscila Castro
+ * @author Maikol Lizano
+ * 
+ * @version 05 diciembre, 2018
  */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
