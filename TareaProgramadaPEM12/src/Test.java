2cdb5bb272a51e6f86b922bcc76edb00d500bd03
prica <prica@LAPTOP-92D9G2RR>
2018-12-05 18:26:30 -0600

    Mensaje.

--- TareaProgramadaPEM12/src/Test.java
+++ TareaProgramadaPEM12/src/Test.java
@@ -12,6 +12,7 @@ public class Test {
 
     public static void main(String[] args) {
         ///HOLI AMIGUITOS! :)PERO POR QUÉ ESA WILA TIENE ESE (){.}() ASI AHHHHH?????? mmm
+        //Priscila.
     }
     
 }
