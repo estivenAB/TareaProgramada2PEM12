871abf56b20d9cb89938f38b86e70e513b1c6b89
prica <prica@LAPTOP-92D9G2RR>
2018-12-05 19:21:43 -0600

    Arreglo de paquetes.

---
+++ TareaProgramadaPEM12/src/main/Test.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ *
+ * @author Estiven Álvarez
+ * @author Priscila Castro
+ * @author Maikol Lizano
+ * 
+ * @version 05 diciembre, 2018
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ///HOLI AMIGUITOS! :)PERO POR QUÉ ESA WILA TIENE ESE (){.}() ASI AHHHHH?????? mmm
+        //Priscila.
+        //ME PICA UN HUEVITO
+    }
+    
+}
